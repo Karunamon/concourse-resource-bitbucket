b5d8a3c12396336dca694aa092a0a1097f316f48
Michael Parks <tk@tkware.info>
2016-05-17 08:03:57 -0600

    Fix various typos and unnecessary repetitions

--- scripts/bitbucket.py
+++ scripts/bitbucket.py
@@ -10,7 +10,7 @@ from requests.auth import HTTPBasicAuth
 
 # Convenience method for writing to stderr. Coerces input to a string.
 def err(txt):
-    sys.stderr.write(str(txt))
+    sys.stderr.write(str(txt) + "\n")
 
 
 # Convenience method for pretty-printing JSON
@@ -19,14 +19,15 @@ def json_pp(json_object):
         json.dumps(json_object,
                    sort_keys=True,
                    indent=4,
-                   separators=(',', ':') + "\n")
+                   separators=(',', ':')) + "\n"
     elif isinstance(json_object, str):
         json.dumps(json.loads(json_object),
                    sort_keys=True,
                    indent=4,
-                   separators=(',', ':') + "\n")
+                   separators=(',', ':')) + "\n"
     else:
         raise NameError('Must be a dictionary or json-formatted string')
+
 
 # Check and in are useless for this resource, so just return blank objects
 if 'check' in sys.argv[0]:
@@ -35,7 +36,6 @@ if 'check' in sys.argv[0]:
 elif 'in' in sys.argv[0]:
     print('{}')
     sys.exit(0)
-
 
 j = json.loads(sys.stdin.read())
 
@@ -50,21 +50,21 @@ build_status = j['params']['build_status']
 artifact_dir = "%s/%s" % (sys.argv[1], j['params']['repo'])
 
 if debug:
-    err("--DEBUG MODE--\n")
+    err("--DEBUG MODE--")
 
 # It is recommended not to parse the .git folder directly due to garbage
 # collection. It's more sustainable to just install git and parse the output.
 commit_sha = subprocess.check_output(
-    ['git', '-C', artifact_dir, 'rev-parse', 'HEAD']
-    ).strip()
+        ['git', '-C', artifact_dir, 'rev-parse', 'HEAD']
+).strip()
 
 if debug:
-    err("Commit: " + commit_sha + "\n")
+    err("Commit: " + str(commit_sha))
 
 # The build status can only be one of three things
 if 'INPROGRESS' not in build_status and \
-    'SUCCESSFUL' not in build_status and \
-        'FAILED' not in build_status:
+                'SUCCESSFUL' not in build_status and \
+                'FAILED' not in build_status:
     err("Invalid build status, must be: INPROGRESS, SUCCESSFUL, or FAILED")
     exit(1)
 
@@ -77,15 +77,15 @@ if verify_ssl is False:
 # Construct the URL and JSON objects
 post_url = url + commit_sha
 if debug:
-    err(str(j) + "\n")
-    err("Notifying %s that build %s is in status: %s\n" %
+    err(json_pp(j))
+    err("Notifying %s that build %s is in status: %s" %
         (post_url, os.environ["BUILD_NAME"], build_status))
 
 build_url = "{url}/pipelines/{pipeline}/jobs/{jobname}/builds/{buildname}".format(
-    url=os.environ['ATC_EXTERNAL_URL'],
-    pipeline=os.environ['BUILD_PIPELINE_NAME'],
-    jobname=os.environ['BUILD_JOB_NAME'],
-    buildname=os.environ['BUILD_NAME'],
+        url=os.environ['ATC_EXTERNAL_URL'],
+        pipeline=os.environ['BUILD_PIPELINE_NAME'],
+        jobname=os.environ['BUILD_JOB_NAME'],
+        buildname=os.environ['BUILD_NAME'],
 )
 
 # https://developer.atlassian.com/bitbucket/server/docs/latest/how-tos/updating-build-status-for-commits.html
@@ -98,33 +98,32 @@ js = {
 }
 
 if debug:
-    err(str(js) + "\n")
+    err(json_pp(js))
 
 r = requests.post(
-    post_url,
-    auth=HTTPBasicAuth(username, password),
-    verify=verify_ssl,
-    json=js)
+        post_url,
+        auth=HTTPBasicAuth(username, password),
+        verify=verify_ssl,
+        json=js)
 
 if debug:
-    err("Request result: " + str(r) + "\n")
-
+    err("Request result: " + str(r))
 
 if r.status_code == 403:
-    err("HTTP 403 Forbidden - Does your bitbucket user have rights to the repo?\n")
+    err("HTTP 403 Forbidden - Does your bitbucket user have rights to the repo?")
     sys.exit(2)
 elif r.status_code == 401:
-    err("HTTP 401 Unauthorized - Are your bitbucket credentials correct?\n")
+    err("HTTP 401 Unauthorized - Are your bitbucket credentials correct?")
     sys.exit(2)
 
 # All other errors, just dump the JSON
 if r.status_code != 204:  # 204 is a success per Bitbucket docs
-    err(json.pp(r.json()))
+    err(json_pp(r.json()))
     sys.exit(2)
 
 status_js = {"version": {"ref": commit_sha}}
 
 if debug:
-    err("Returning to concourse:\n" + json_pp(status_js) + "\n")
+    err("Returning to concourse:\n" + json_pp(status_js))
 
 print(json.dumps(status_js))
